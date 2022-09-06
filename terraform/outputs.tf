6df6ad848444058c9a1e883f22e9321a321cb2fb
Daniel Arnauer <daniel.arnauer@accenture.com>
2022-09-06 13:26:54 +0200

    Add Terraform

---
+++ terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "function_app_name" {
+  value       = azurerm_linux_function_app.my_linux_function_app.name
+  description = "Deployed function app name"
+}
+
+output "function_app_default_hostname" {
+  value       = azurerm_linux_function_app.my_linux_function_app.default_hostname
+  description = "Deployed function app hostname"
+}
